39f8246956ae6cc7577323d41b0e1b8279efd25e
Yichuan Shi <yichuan.shi@outlook.com>
2019-07-04 13:29:20 +0100

    init

---
+++ qa.py
@@ -0,0 +1,13 @@
+# QA for WDPA 2019
+# Library for checks
+
+
+# check values in the field, against all valid values
+def check_attribute(field, valid_values):
+    pass
+
+def count_unique_values(field):
+    pass
+
+def check_case(field):
+    pass
