fc53e4f8f39d29046ba349a332a0f38a384381d6
stijnwcmc <51745583+stijn-wcmc@users.noreply.github.com>
2019-08-01 16:38:51 +0100

    Added main function point.py. Added documentation for point.py and poly.py

---
+++ point.py
@@ -0,0 +1,35 @@
+# Load packages and modules
+import sys, arcpy
+from wdpa.qas import arcgis_table_to_df, find_wdpa_rows, pt_checks, INPUT_FIELDS_PT, invalid_data_import
+from wdpa.stijn import output_errors_to_excel
+
+# Load input
+input_pt = sys.argv[1]
+output_path = sys.argv[2]
+
+# Let us begin
+arcpy.AddMessage('\nAll hail the WDPA\n')
+
+# Convert Point table to pandas DataFrame
+arcpy.AddMessage('Converting to pandas DataFrame')
+pt_df = arcgis_table_to_df(input_pt, INPUT_FIELDS_PT)
+result = dict()
+
+# Verify whether data import is correct
+if invalid_data_import(pt_df, INPUT_FIELDS_PT):
+        arcpy.AddMessage('ERROR: the list of fields in the Point table is incorrect')
+        sys.exit()
+
+# Run the checks
+arcpy.AddMessage('--- Running QA checks on Points ---')
+for pt_check in pt_checks: # pt_checks is a dictionary with checks' descriptive names and function names
+    arcpy.AddMessage('Running:' + pt_check['name'])
+    # checks are not currently optimised, thus return all pids regardless
+    wdpa_pid = pt_check['func'](pt_df, True)
+
+    # For each check, obtain the rows that contain errors
+    if wdpa_pid.size > 0:
+        result[pt_check['name']] = find_wdpa_rows(pt_df, wdpa_pid)
+
+output_errors_to_excel(result, output_path, pt_checks)
+arcpy.AddMessage('The QA checks have finished. \n\nWritten by Stijn den Haan and Yichuan Shi\nAugust 2019')
--- poly.py
+++ poly.py
@@ -1,23 +1,35 @@
+# Load packages and modules
 import sys, arcpy
-from wdpa.qas import arcgis_table_to_df, find_wdpa_rows, poly_checks, INPUT_FIELDS_POLY
+from wdpa.qas import arcgis_table_to_df, find_wdpa_rows, poly_checks, INPUT_FIELDS_POLY, invalid_data_import
 from wdpa.stijn import output_errors_to_excel
 
-# load input
+# Load input
 input_poly = sys.argv[1]
 output_path = sys.argv[2]
 
-# poly
-arcpy.AddMessage('Converting to pandas dataframe')
+# Let us begin
+arcpy.AddMessage('\nAll hail the WDPA\n')
+
+# Convert Polygon table to pandas DataFrame
+arcpy.AddMessage('Converting to pandas DataFrame')
 poly_df = arcgis_table_to_df(input_poly, INPUT_FIELDS_POLY)
 result = dict()
 
+# Verify whether data import is correct. If not, exit the programme.
+if invalid_data_import(poly_df, INPUT_FIELDS_POLY):
+        arcpy.AddMessage('ERROR: the list of fields in the Point table is incorrect')
+        sys.exit()
+
+# Run the checks
 arcpy.AddMessage('--- Running QA checks ---')
-for poly_check in poly_checks:
+for poly_check in poly_checks: # poly_checks is a dictionary with checks' descriptive names and function names
     arcpy.AddMessage('Running:' + poly_check['name'])
     # checks are not currently optimised, thus return all pids regardless
     wdpa_pid = poly_check['func'](poly_df, True)
 
+    # For each check, obtain the rows that contain errors
     if wdpa_pid.size > 0:
         result[poly_check['name']] = find_wdpa_rows(poly_df, wdpa_pid)
 
 output_errors_to_excel(result, output_path, poly_checks)
+arcpy.AddMessage('The QA checks have finished. \n\nWritten by Stijn den Haan and Yichuan Shi\nAugust 2019')
